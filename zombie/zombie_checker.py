e3b673b78a004a63e378ab83dd5ddb9359b7f1fb
bambielli <brian@synd.io>
2015-08-29 16:32:08 -0500

    small updates to the scraper comments

--- zombie/zombie_checker.py
+++ zombie/zombie_checker.py
@@ -1,17 +1,17 @@
 import urllib.request
 
 """
-This script will be run by the cronjob every 5 minutes, to check and make sure that zombie is available for purchase.
+This script will be run by the cronjob every X minutes,
+to check and make sure that zombie is available for purchase at the brewpub.
 """
 
 def main():
-	beer_string = str(urllib.request.urlopen("http://www.3floyds.com/beers-to-go/").read())
+	beer_response = urllib.request.urlopen("http://www.3floyds.com/beers-to-go/")
 
-	# find the position of the FAQs string, and delete everything after it in the response.
-	# the current beer offerings are before the FAQs section.
-	delete_index_post = beer_string.index('22-OZ')
+	# cut the response string down to the section where they list the available beers for pickup
 	delete_index_pre = beer_string.index('6-Packs')
-	beer_string = beer_string[delete_index_pre:delete_index_post]
+	delete_index_post = beer_string.index('22-OZ')
+	beer_string = str(beer_response.read())[delete_index_pre:delete_index_post]
 
 	if 'zombie' in beer_string or 'Zombie' in beer_string:
 		return 0
@@ -22,3 +22,4 @@ if not main():
 	print ('Zombie')
 else:
 	print ('No Zombie')
+
