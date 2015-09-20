5c284c73fa2870affd05d18935cf44e34fa8c005
bambielli <brian@synd.io>
2015-09-20 14:17:42 -0500

    changing database config so it connects to psql

--- zombie/settings.py
+++ zombie/settings.py
@@ -89,13 +89,8 @@ WSGI_APPLICATION = 'zombie.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': dj_database_url.config()
 }
-
-DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
